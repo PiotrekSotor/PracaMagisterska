ff9e8e6b6463c78dbfe02759b5a70a337d51ee2b
PiotrekSotor <piotrsotor@gmail.com>
2017-05-10 16:34:24 +0200

    clean function testWithTimeMeasurement

--- repo/R/workspace/ga_test.R
+++ repo/R/workspace/ga_test.R
@@ -5,8 +5,8 @@ my_gaMonitor <- function(object, digits = getOption("digits"), ...){
   elapsed<-toString(  round( x = ((endTime-startTime)["elapsed"]) , digits = 1)  )
   
   fitness <- na.exclude(object@fitness)
-  # cat(numberOfRun,elapsed)
-  # cat(resultsInTime[numberOfRun,elapsed,"bestSolution"])
+  cat(numberOfRun,elapsed)
+  cat(resultsInTime[numberOfRun,elapsed,"bestSolution"])
   resultsInTime[numberOfRun,elapsed,"bestSolution"] <<- max(fitness)
   resultsInTime[numberOfRun,elapsed,"meanSolution"] <<- mean(fitness)
   
@@ -19,57 +19,54 @@ my_gaMonitor <- function(object, digits = getOption("digits"), ...){
   flush.console()
 }
 
-iterationsSoFar<-0
-iterations<-10
-allIterationNumber<-100*iterations
 
-possibleTimes <- seq(0,20, by=0.1)
-numberOfRun <- seq(1,iterations, by=1)
-collectedValues<-c("bestSolution", "meanSolution")
-
-
-resultsInTime<- array(dimnames = list(numberOfRun = numberOfRun, time=possibleTimes, values=collectedValues),
-                      dim = c(length(numberOfRun),length(possibleTimes), length(collectedValues))
-                      )
-avrInTime<- array(dimnames = list(time=possibleTimes, values=collectedValues),
-                      dim = c(length(possibleTimes), length(collectedValues))
-)
-
-
-for (i in 1:iterations){
-  numberOfRun<-i
-  startTime<-proc.time()
+testWithTimeMeasurment <- function (memetic=TRUE, fun, numOfRuns = 10, iters=100, popSize=100, poptim=0.05, pressel=.5, optimiseForMinimal = TRUE){
+  iterationsSoFar<<-0
+  allIterationNumber<<-iters*numOfRuns
   
-  ga(type = "real-valued", 
-                 fitness = McCormic, 
-                 min = c(-5,-5), max=c(5,5),
-                 monitor = my_gaMonitor, 
-                maxiter = 200,
-     popSize = 150,
-                 optimArgs = list(method="L-BFGS-B",
-                                  poptim=1,
-                                  pressel = 0.5,
-                                  control=list(fnscale=-1)),
-                 optim = FALSE
-     )
-}
-
-for (timestamp in 1:length(possibleTimes)){
-  for (value in 1:length(collectedValues)){
-    avrInTime[timestamp,value] <- mean(resultsInTime[,timestamp,value], na.rm = TRUE)
+  possibleTimes <- seq(0,20, by=0.1)
+  numberOfRun <- seq(1,numOfRuns, by=1)
+  collectedValues<-c("bestSolution", "meanSolution")
+  
+  
+  resultsInTime<<- array(dimnames = list(numberOfRun = numberOfRun, time=possibleTimes, values=collectedValues),
+                        dim = c(length(numberOfRun),length(possibleTimes), length(collectedValues))
+                        )
+  avrInTime<- array(dimnames = list(time=possibleTimes, values=collectedValues),
+                        dim = c(length(possibleTimes), length(collectedValues))
+  )
+  
+  
+  for (i in 1:numOfRuns){
+    numberOfRun<<-i
+    startTime<<-proc.time()
+    
+    ga(type = "real-valued", 
+                   fitness = fun, 
+                   min = c(-5,-5), max=c(5,5),
+                   monitor = my_gaMonitor, 
+                   maxiter = iters,
+       popSize = popSize,
+                   optimArgs = list(method="L-BFGS-B",
+                                    poptim=poptim,
+                                    pressel = pressel,
+                                    control=list(fnscale=ifelse(isTRUE(optimiseForMinimal),-1,1))
+                                    ),
+                   optim = memetic
+       )
   }
+  
+  for (timestamp in 1:length(possibleTimes)){
+    for (value in 1:length(collectedValues)){
+      avrInTime[timestamp,value] <- mean(resultsInTime[,timestamp,value], na.rm = TRUE)
+    }
+  }
+  
+  rm("numberOfRun", envir = globalenv())
+  rm("allIterationNumber", envir = globalenv())
+  rm("resultsInTime", envir = globalenv())
+  rm("iterationsSoFar", envir = globalenv())
+  rm("startTime", envir = globalenv())
+  
+  return (avrInTime)
 }
-
-# new function for monitoring within RStudio
-# gaMonitor <- function(object, digits = getOption("digits"), ...)
-# { 
-#   fitness <- na.exclude(object@fitness)
-#   sumryStat <- c(mean(fitness), max(fitness))
-#   sumryStat <- format(sumryStat, digits = digits)
-#   replicate(2, clearConsoleLine()) 
-#   cat(paste("\rGA | iter =", object@iter, "\n")) 
-#   cat(paste("Mean =", sumryStat[1], "| Best =", sumryStat[2], "\n"))
-#   flush.console()
-# }
-
-
