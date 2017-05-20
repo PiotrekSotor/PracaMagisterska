0e375edee61593e010c426a66d53f349ccf8751d
PiotrekSotor <piotrsotor@gmail.com>
2017-05-20 16:53:56 +0200

    [Java] fixed pom.xml

--- repo/Java/FirstRJavaProj/src/main/First.java
+++ repo/Java/FirstRJavaProj/src/main/First.java
@@ -1,43 +1,29 @@
 package main;
 
-import java.nio.file.Paths;
-
-import org.junit.internal.runners.ErrorReportingRunner;
-import org.rosuda.JRI.Rengine;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import guiInterface.AbstractController;
-import guiInterface.Controller;
 import guiInterface.MainFrame;
-import interfaces.AbstractJavaInvoker;
-import interfaces.Java_plotFunction3DInvoker;
-import interfaces.REngineService;
-import interfaces.RMainLoopCallbacks;
 
 public class First {
-	
-	public static void main (String a[]){
 
+	public static void main(String a[]) {
 
-//		REngineService service = new REngineService();
-//		
-//		Java_plotFunction3DInvoker plot = new Java_plotFunction3DInvoker();
-//		plot.withApplicableFunction(ApplicableFunction.CEC2013_9)
-//			.withDestinationPath("D:/plot.png")
-//			.withXRange(-10f, 5f, 0.1f)
-//			.withYRange(-5f, 4f, 1.5f);
-//		print(plot.getRCommand());
-//		service.invokeOnStaticInstance(plot);
-//		
-//		service.shutDownR();
+		// REngineService service = new REngineService();
+		//
+		// Java_plotFunction3DInvoker plot = new Java_plotFunction3DInvoker();
+		// plot.withApplicableFunction(ApplicableFunction.CEC2013_9)
+		// .withDestinationPath("D:/plot.png")
+		// .withXRange(-10f, 5f, 0.1f)
+		// .withYRange(-5f, 4f, 1.5f);
+		// print(plot.getRCommand());
+		// service.invokeOnStaticInstance(plot);
+		//
+		// service.shutDownR();
 
-		
 		MainFrame frame = new MainFrame();
-//		frame.repaintWithImage("P:/Pwr_projects/PracaMagisterska/PracaMagisterska/repo/R/workspace/porownanieOperatorow_17.png");
+		// frame.repaintWithImage("P:/Pwr_projects/PracaMagisterska/PracaMagisterska/repo/R/workspace/porownanieOperatorow_17.png");
 
 	}
-	
-	private static void print(String s){
+
+	private static void print(String s) {
 		System.out.println(s);
 	}
 }
