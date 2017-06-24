375fe219e555c30091ba1097851d3288fe421671
PiotrekSotor <piotrsotor@gmail.com>
2017-06-24 18:11:48 +0200

    [R] implementacja scenariuszy doboru parametrow

---
+++ repo/R/workspace/parameter selection scenarios/optimiseUsingTriples.R
@@ -0,0 +1,123 @@
+select123parameters <- function(fun, lower_bounds, upper_bounds, 
+                                par_4 = 0.05, par_5 = 0.5, par_6 = "L-BFGS-B", 
+                                memetic_enabled=TRUE){
+  
+  num_of_tests <- 30
+  
+  par_1_set <- seq(0.5, 1, by=.1)
+  par_2_set <- seq(0.5, 1, by=.1)
+  par_3_set <- seq(25,  100, by=25)
+  
+  result_pack <- array(dimnames = list(pMutation=par_1_set, pCrossover=par_2_set, popSize=par_3_set),
+                      dim = c(length(par_1_set),length(par_2_set),length(par_3_set)))
+  start_time <- proc.time()  
+  for (par_1 in par_1_set){
+    for (par_2 in par_2_set){
+      for(par_3 in par_3_set){
+        best_solution <- 0
+        X <- sprintf("%s %s %s", par_1,par_2,par_3)
+        message(X)
+        for(test in 1:num_of_tests){
+          result <- ga(fitness = fun, type = "real-valued", 
+                       min = lower_bounds, 
+                       max = upper_bounds,
+                       maxiter = 5,
+                       pmutation = par_1,
+                       pcrossover = par_2,
+                       popSize = par_3,
+                       optim = memetic_enabled,
+                       optimArgs = list(poptim=par_4,
+                                        pressel=par_5,
+                                        method=par_6,
+                                        control=list(fnscale=-1, maxit=2))
+              )
+          
+          
+          
+          best_solution <- best_solution + (-result@fitnessValue)
+        }
+        result_pack[toString(par_1), toString(par_2), toString(par_3)] = best_solution/num_of_tests
+      }
+    }
+  }
+  stop_time <- proc.time() - start_time
+  
+  best_par_3_fitness = min(result_pack[,,1])
+  best_par_3_value = par_3_set[1]
+  for (par_3 in par_3_set){
+    if (min(result_pack[,,toString(par_3)]) < best_par_3_fitness){
+      best_par_3_value = par_3   
+    }
+  }
+  par_1_par_2_indexes <- getIndexesOfOptim(result_pack[,,toString(best_par_3_value)], maxSearch = FALSE)
+  selected_params_values <- c(par_1_set[par_1_par_2_indexes[1]], par_2_set[par_1_par_2_indexes[2]], best_par_3_value)
+  
+  
+  return (list(time=stop_time, params=selected_params_values, best_fitness = min(result_pack)))
+}
+
+########################################################################## 
+
+select456parameters <- function(fun, lower_bounds, upper_bounds, 
+                                par_1 = 0.1, par_2 = 0.8, par_3 = 50, 
+                                memetic_enabled=TRUE){
+  
+  num_of_tests <- 20
+  
+  par_4_set <- seq(0.5, 1, by=.1)
+  par_5_set <- seq(0.5, 1, by=.1)
+  par_6_set <- c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN")
+  
+  result_pack <- array(dimnames = list(poptim=par_4_set, pressel=par_5_set, method=par_6_set),
+                       dim = c(length(par_4_set),length(par_5_set),length(par_6_set)))
+  start_time <- proc.time()  
+  for (par_4 in par_4_set){
+    for (par_5 in par_5_set){
+      for(par_6 in par_6_set){
+        best_solution <- 0
+        X <- sprintf("%s %s %s", par_4,par_5,par_6)
+        message(X)
+        for(test in 1:num_of_tests){
+          result <- ga(fitness = fun, type = "real-valued", 
+                       min = lower_bounds, 
+                       max = upper_bounds,
+                       maxiter = 5,
+                       pmutation = par_1,
+                       pcrossover = par_2,
+                       popSize = par_3,
+                       optim = memetic_enabled,
+                       optimArgs = list(poptim=par_4,
+                                        pressel=par_5,
+                                        method=par_6,
+                                        control=list(fnscale=-1, maxit=2))
+          )
+          
+          
+          
+          best_solution <- best_solution + (-result@fitnessValue)
+        }
+        result_pack[toString(par_4), toString(par_5), toString(par_6)] = best_solution/num_of_tests
+      }
+    }
+  }
+  stop_time <- proc.time() - start_time
+  
+  best_par_6_fitness = min(result_pack[,,1])
+  best_par_6_value = par_6_set[1]
+  for (par_6 in par_6_set){
+    if (min(result_pack[,,toString(par_6)]) < best_par_6_fitness){
+      best_par_6_value = par_6   
+    }
+  }
+  par_4_par_5_indexes <- getIndexesOfOptim(result_pack[,,toString(best_par_6_value)], maxSearch = FALSE)
+  selected_params_values <- c(par_4_set[par_4_par_5_indexes[1]], par_5_set[par_4_par_5_indexes[2]], best_par_6_value)
+  
+  
+  return (list(time=stop_time, params=selected_params_values, best_fitness = min(result_pack)))
+}
+
+
+# paviani123_test <- select456parameters(fun = Paviani
+#                     , lower_bounds = PavianiBounds$lower+0.0001
+#                     , upper_bounds = PavianiBounds$upper-0.0001)
+
---
+++ repo/R/workspace/parameter selection scenarios/performOptimisationScenarios.R
@@ -0,0 +1,92 @@
+definedFuntion <- Paviani
+definedBounds <- PavianiBounds
+
+# scenario $[1-2,\; 4-5,\; 3-6]
+first_selection_12_45_36 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                    par_1 = NULL, par_2 = NULL)
+
+second_selection_12_45_36 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_1 = first_selection_12_45_36$params[1], par_2 = first_selection_12_45_36$params[2]
+                                       , par_4 = NULL, par_5 = NULL)
+
+third_selection_12_45_36 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                        par_1 = first_selection_12_45_36$params[1], par_2 = first_selection_12_45_36$params[2]
+                                       , par_4 = second_selectio_12_45_36n$params[1], par_5 = second_selection_12_45_36$params[2]
+                                       , par_3 = NULL, par_6 = NULL)
+scenario_12_45_36_params <- c(first_selection$params, second_selection$params, third_selection$params)
+scenario_12_45_36_time <- c(first_selection_12_45_36$time, second_selection_12_45_36$time, third_selection_12_45_36$time)
+
+# scenario $[3-6,\; 1-2,\; 4-5]$
+
+first_selection_36_12_45 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_3 = NULL, par_6 = NULL)
+
+second_selection_36_12_45 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                        par_3 = first_selection_36_12_45$params[1], par_6 = first_selection_36_12_45$params[2]
+                                        , par_1 = NULL, par_2 = NULL)
+
+third_selection_36_12_45 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_3 = first_selection_36_12_45$params[1], par_6 = first_selection_36_12_45$params[2]
+                                       , par_1 = second_selection_36_12_45$params[1], par_2 = second_selection_36_12_45$params[2]
+                                       , par_4 = NULL, par_5 = NULL)
+scenario_36_12_45_params <- c(first_selection_36_12_45$params, second_selection_36_12_45$params, third_selection_36_12_45$params)
+scenario_36_12_45_time <- c(first_selection_36_12_45$time, second_selection_36_12_45$time, third_selection_36_12_45$time)
+
+
+# scenario $[4-5,\; 3-6,\; 1-2]$
+
+first_selection_45_36_12 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_4 = NULL, par_5 = NULL)
+
+second_selection_45_36_12 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                        par_4 = first_selection_45_36_12$params[1], par_5 = first_selection_45_36_12$params[2]
+                                        , par_3 = NULL, par_6 = NULL)
+
+third_selection_45_36_12 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_4 = first_selection_45_36_12$params[1], par_5 = first_selection_45_36_12$params[2]
+                                       , par_3 = second_selection_45_36_12$params[1], par_6 = second_selection_45_36_12$params[2]
+                                       , par_1 = NULL, par_2 = NULL)
+scenario_45_36_12_params <- c(first_selection_45_36_12$params, second_selection_45_36_12$params, third_selection_45_36_12$params)
+scenario_45_36_12_time <- c(first_selection_45_36_12$time, second_selection_45_36_12$time, third_selection_45_36_12$time)
+
+# scenario $[1-6,\; 2-4,\; 3-5]$
+
+first_selection_16_24_35 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_1 = NULL, par_6 = NULL)
+
+second_selection_16_24_35 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                        par_1 = first_selection_16_24_35$params[1], par_6 = first_selection_16_24_35$params[2]
+                                        , par_2 = NULL, par_4 = NULL)
+
+third_selection_16_24_35 <- selectTwoparameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                       par_1 = first_selection_16_24_35$params[1], par_6 = first_selection_16_24_35$params[2]
+                                       , par_2 = second_selection_16_24_35$params[1], par_4 = second_selection_16_24_35$params[2]
+                                       , par_3 = NULL, par_5 = NULL)
+scenario_16_24_35_params <- c(first_selection_16_24_35$params, second_selection_16_24_35$params, third_selection_16_24_35$params)
+scenario_16_24_35_time <- c(first_selection_16_24_35$time, second_selection_16_24_35$time, third_selection_16_24_35$time)
+
+
+# scenario\item $[1-2-3,\; 4-5-6]$
+
+first_selection_123_456 <- select123parameters(fun=defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-.0001)
+
+second_selection_123_456 <- select456parameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                                 par_1 = first_selection_123_456$params[1], 
+                                                par_2 = first_selection_123_456$params[2],
+                                                par_3 = first_selection_123_456$params[3])
+
+scenario_123_456_params <- c(first_selection_123_456$params, second_selection_123_456$params)
+scenario_123_456_time <- c(first_selection_123_456$time, second_selection_123_456$time)
+
+
+# scenario  \item $[4-5-6,\; 1-2-3]$
+
+first_selection_465_123 <- select456parameters(fun=defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-.0001)
+
+second_selection_465_123 <- select123parameters(fun = defindedFunction, lower_bounds = definedBounds$lower+.0001, upper_bounds = definedBounds$upper-0.0001,
+                                                par_4 = first_selection_123_456$params[1], 
+                                                par_5 = first_selection_123_456$params[2],
+                                                par_6 = first_selection_123_456$params[3])
+
+scenario_465_123_params <- c(first_selection_465_123$params, second_selection_465_123$params)
+scenario_465_123_time <- c(first_selection_465_123$time, second_selection_465_123$time)
